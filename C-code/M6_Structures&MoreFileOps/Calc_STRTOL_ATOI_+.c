c5a1940529ee2662a912b6230c2c460f0f706b11
AdaptiveResonance <105830013+AdaptiveResonance@users.noreply.github.com>
2023-02-11 16:24:22 -0700

    updated modules names and cleaned up

--- C-code/M6_Structures&MoreFileOps/Calc_STRTOL_ATOI_+.c
+++ C-code/M6_Structures&MoreFileOps/Calc_STRTOL_ATOI_+.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-//CMD adder/calc using strtol or atoi comparison
+//CMDline adder/calc using strtol and/or atoi comparison
  
+void addsit2(char **argv, int argc, int *num, int *sum)
+{
+     for(int idx = 1; idx < argc; idx += 2)
+    {
+        printf("%s: %d\n", argv[idx], *argv[idx]);
+        num[idx/2] = strtol(argv[idx],0,10);   // strtol converts character array to integer
+        printf("\tnum[%d] = %d\n", idx/2, num[idx/2]);
+        sum += num[idx/2];
+    }
+}
 void addsit(char **argv, int argc, int *num, int *sum)
 {
     for(int idx = 1; idx < argc; idx += 2) //go up by twos
@@ -22,16 +32,11 @@ int main(int argc, char *argv[])
     //sum = addsit(argv, argc, num);
     printf("\t ---- Sum is %d\n ----", sum);
     
-    sum = 0;
-    for(int idx = 1; idx < argc; idx += 2)
-    {
-        printf("%s: %d\n", argv[idx], *argv[idx]);
-        num[idx/2] = strtol(argv[idx],0,10);   // strtol converts character array to integer
-        printf("\tnum[%d] = %d\n", idx/2, num[idx/2]);
-        sum += num[idx/2];
-    }
-    printf("Total sum is: %d\n", sum);
-    printf("%s\n", argv[1]); // We are printing a string. is because the argv[1] is an address
-    //printf("Sum of %d number is: %d\n", argc - 1, sum);
+    sum = 0;//zero out sum
+	addsit2(argv, argc, num, &sum);
+	printf("Total sum is: %d\n", sum);
+    
+	printf("%s\n", argv[1]); // We are printing a string. The argv[1] is an address
+    //printf("Sum of %d numbers is: %d\n", argc - 1, sum);
     return 0;
 }
