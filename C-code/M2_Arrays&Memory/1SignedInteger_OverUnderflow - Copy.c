6b345b4f089a216e7540b76672e47bcfe50fa507
AdaptiveResonance <105830013+AdaptiveResonance@users.noreply.github.com>
2023-03-22 23:07:01 -0600

    New signed/unsigned integers & overflow/Underflows

---
+++ C-code/M2_Arrays&Memory/1SignedInteger_OverUnderflow - Copy.c
@@ -0,0 +1,26 @@
+// Integer Overflow/Underflow & Signed + Unsigned integers
+// 32-bit system/code compiled as 32 bits, occupies 32 bits of space
+// 2**32 produces 4,294,967,296 possible values
+// so unsigned int should be able to contain 0 to 4,294,967,295 
+// as unsigned int only allows for positive values!
+// Signed int allows for negative integers in a range of 
+// −2,147,483,648 to 2,147,483,647
+// Overflow is essentially applying a value to a datatype
+// that is too big to hold so it "spills over"
+
+#include <stdio.h>
+
+int main()
+    {
+        signed int signed_num = -2147483648;
+        // return sizeof signed integer
+        printf("Bytesize of signed_num is :%d bytes or %d bits \n", sizeof(num_signed), sizeof(num_signed) * 8);
+
+        // Print initial value before change
+        printf("Initial signed_num value is: %u \n", num_signed);
+
+        // Print new value
+        printf("%d - 1 = %d \n", num_signed, num_signed -1);
+        //-2147483648 -1 should equal -2,147,483,649 however returns 2147483648
+        return 0;
+    }
---
+++ C-code/M2_Arrays&Memory/1UnSignedInteger_OverUnderflow.c
@@ -0,0 +1,27 @@
+// Integer Overflow/Underflow & Signed + Unsigned integers
+// 32-bit system/code compiled as 32 bits, occupies 32 bits of space
+// 2**32 produces 4,294,967,296 possible values
+// so unsigned int should be able to contain 0 to 4,294,967,295 
+// as unsigned int only allows for positive values!
+// Signed int allows for negative integers in a range of 
+// −2,147,483,648 to 2,147,483,647
+// Overflow is essentially applying a value to a datatype
+// that is too big to hold so it "spills over"
+
+#include <stdio.h>
+
+int main()
+    {	  
+        unsigned int unsigned_num = 429496725;//maximum number
+
+        // return sizeof unsigned integer
+        printf("Bytesize of unsigned_num is :%d bytes or %d bits \n", sizeof(unsigned_num), sizeof(unsigned_num) * 8);
+
+        // Print initial value before change
+        printf("Initial unsigned_num value is: %u \n", unsigned_num);
+
+        // Print new value
+        printf("%u + 1 = %u \n", unsigned_num, unsigned_num + 1);
+        //4294967295 + 1 = 4294967296 but returns 0 due to overflow
+        return 0;
+    }
