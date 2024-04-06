d5ccf09b4ba15161c397e9c76647c5b795615395
AdaptiveResonance <105830013+AdaptiveResonance@users.noreply.github.com>
2024-04-06 01:14:31 -0600

    Bugfixing

--- C-code/M4_Pointers&Addresses/strcpy.c
+++ C-code/M4_Pointers&Addresses/strcpy.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include <string.h>
 
-//incomplete
+//String copy take a source string and saves it to destination
 void strcpy1 (char *dest, char *src)
 {
-	strcpy(src, "Hola ")
-	strcpy(*dest, *src)//destination first then what we copy second
+	strcpy(src, "Hola ");
+	strcpy(dest, src);
+	//destination first then what we copy second
 }
 int main()
 {
-char array[5]={'H','E','l','L','O','W','o','r','l','d'};
+char array[5]={'H','E','l','L','O'};
 char Narray[5];
 strcpy1(Narray, array);
-printf("new stringis: %s", Narray);
+printf("new string is: %s", Narray);
 }
